5033ee539fdefc4e23139542f3a723df559ba186
Mary Gibbs <mgibbs1259@gmail.com>
2023-07-09 16:00:12 +0000

    update

--- README.md
+++ README.md
@@ -10,7 +10,11 @@ If you don't have Docker already installed, follow these [instructions](https://
 2. Follow the Authentication section [here](<https://github.com/tsightler/ring-mqtt/wiki/Installation-(Docker)>), specifically the Acquire the Refresh Token section that has you run the following command `docker run -it --rm --mount type=bind,source=<absolute path to spring repository>,target=/data --entrypoint /app/ring-mqtt/init-ring-mqtt.js tsightler/ring-mqtt`
 3. Create a copy of .example-env file and call it .env
 4. You will need to modify MQTT_LOCATION_ID and MQTT_DEVICE_IDS in the .env, filling in location ID and device IDs. To get the location ID and device IDs, run `docker compose up` from within the spring directory. Then, inspect the console output and search for `Starting Device Discovery...`. For the location of interest, note the location ID (comes after `New location:` and is in `()`) in the .env file. Then for the devices of interest within the location of interest, note the device IDs (comes after `New device:` and is in `()`) in the .env file. Then, `Ctrl + C` and `docker compose down --rmi all`.
-5. Start spring using `docker compose up -d`
+
+5. Go to `https://app.rach.io/login` and login. Go to `Settings` > `Get API Key`. Then, add this to the .env file. 
+
+
+?. Start spring using `docker compose up -d`
 
 # Development
 
--- run_spring.py
+++ run_spring.py
@@ -3,6 +3,7 @@ import logging
 from dotenv import load_dotenv
 
 import spring.control_ring as cr
+import spring.control_sprinklers as cs
 
 
 if __name__ == "__main__":
@@ -11,18 +12,24 @@ if __name__ == "__main__":
     if os.path.exists(".env"):
         load_dotenv(".env")
 
-    client = cr.connect_to_mqtt_broker(
-        os.environ["MQTT_CLIENT_ID"],
-        os.environ["MQTT_USERNAME"],
-        os.environ["MQTT_PASSWORD"],
-        os.environ["MQTT_BROKER"],
-        os.environ["MQTT_PORT"],
+    sprinklers = cs.conenct_to_sprinklers(os.environ["RACHIO_API_KEY"])
+    cs.list_sprinkler_information(sprinklers)
+    cs.start_sprinklers(
+        sprinklers, os.environ["RACHIO_ZONE_IDS"], os.environ["RACHIO_SECONDS"]
     )
 
-    client = cr.subscribe_mqtt_broker_to_topic(
-        client, os.environ["MQTT_LOCATION_ID"], os.environ["MQTT_DEVICE_IDS"]
-    )
+    # client = cr.connect_to_mqtt_broker(
+    #     os.environ["MQTT_CLIENT_ID"],
+    #     os.environ["MQTT_USERNAME"],
+    #     os.environ["MQTT_PASSWORD"],
+    #     os.environ["MQTT_BROKER"],
+    #     os.environ["MQTT_PORT"],
+    # )
 
-    client = cr.set_mqtt_broker_response_to_message(client)
+    # client = cr.subscribe_mqtt_broker_to_topic(
+    #     client, os.environ["MQTT_LOCATION_ID"], os.environ["MQTT_DEVICE_IDS"]
+    # )
 
-    client.loop_forever()
+    # client = cr.set_mqtt_broker_response_to_message(client)
+
+    # client.loop_forever()
